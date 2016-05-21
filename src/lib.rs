18c57c6bc6567cc1e813e0d9fb412374aaee3461
David Henningsson <diwic@ubuntu.com>
2016-05-21 15:35:10 +0200

    Add &mut T to RBA, rename to RMBA
    Signed-off-by: David Henningsson <diwic@ubuntu.com>

--- src/lib.rs
+++ src/lib.rs
@@ -7,58 +7,95 @@ use std::ops::{Deref, DerefMut};
 use std::sync::atomic::AtomicUsize;
 use std::fmt;
 
-/// RBA can store an &T, a Box<T> or an Arc<T>, within just a pointer -
+/// RMBA can store an &T, a &mut T, a Box<T> or an Arc<T>, within just a pointer -
 /// this will panic if you try to store something that's not 32 bit aligned.
-pub struct RBA<'a, T: 'a>(usize, PhantomData<Option<&'a T>>,
+pub struct RMBA<'a, T: 'a>(usize, PhantomData<Option<&'a T>>, PhantomData<Option<&'a mut T>>,
     PhantomData<Option<Box<T>>>, PhantomData<Option<Arc<T>>>);
 
-impl<'a, T: 'a> RBA<'a, T> {
-    fn make(p: *const T, add: usize) -> RBA<'a, T> {
+impl<'a, T: 'a> RMBA<'a, T> {
+    fn make(p: *const T, add: usize) -> RMBA<'a, T> {
         let z: usize = unsafe { transmute(p) };
         assert!(align_of::<T>() >= 4);
         debug_assert!(z & 3 == 0);
-        RBA(z+add, PhantomData, PhantomData, PhantomData)
+        RMBA(z+add, PhantomData, PhantomData, PhantomData, PhantomData)
     }
     
-    pub fn new<A: Into<RBA<'a, T>>>(t: A) -> RBA<'a, T> { t.into() }
+    pub fn new<A: Into<RMBA<'a, T>>>(t: A) -> RMBA<'a, T> { t.into() }
 
-    pub fn new_box(t: T) -> RBA<'static, T> { Box::new(t).into() }
+    pub fn new_box(t: T) -> RMBA<'a, T> { Box::new(t).into() }
+
+    /// Will return a clone if it contains a Arc<T> or &T, or None if it is a Box<T> or &mut T 
+    pub fn try_clone(&self) -> Option<RMBA<'a, T>> {
+        match self.0 & 3 {
+            0 => Some(RMBA(self.0, self.1, self.2, self.3, self.4)),
+            2 => unsafe {
+                let t: Arc<T> = transmute(self.0 - 2 - 2*size_of::<AtomicUsize>());
+                let t2 = t.clone();
+                forget(t);
+                Some(t2.into())
+            },
+            _ => None,
+        }
+    }
+
+    /// Will return a &mut to inner contents if it contains a &mut T, a Box<T> or if the Arc<T> is unique. Otherwise returns None.
+    pub fn get_mut(&mut self) -> Option<&mut T> {
+        match self.0 & 3 {
+            1 => unsafe {
+                let mut b = Box::<T>::from_raw(transmute(self.0-1));
+                let z: *mut T = &mut *b;
+                forget(b);
+                Some(&mut *z)
+            },
+            2 => unsafe {
+                let mut t: Arc<T> = transmute(self.0 - 2 - 2*size_of::<AtomicUsize>());
+                let z: Option<*mut T> = Arc::get_mut(&mut t).map(|z| -> *mut T { &mut *z });
+                forget(t);
+                z.map(|z| &mut *z)
+            },
+            3 => unsafe { Some(transmute(self.0 - 3)) },
+            _ => None,
+        }
+    }
 }
 
 
-impl<'a, T: 'a> From<&'a T> for RBA<'a, T> {
-    fn from(t: &'a T) -> RBA<'a, T> { RBA::make(t as *const T, 0) }
+impl<'a, T: 'a> From<&'a T> for RMBA<'a, T> {
+    fn from(t: &'a T) -> RMBA<'a, T> { RMBA::make(t as *const T, 0) }
 }
 
-impl<'a, T: 'a> From<Box<T>> for RBA<'a, T> {
-    fn from(t: Box<T>) -> RBA<'a, T> { RBA::make(Box::into_raw(t) as *const T, 1) }
+impl<'a, T: 'a> From<&'a mut T> for RMBA<'a, T> {
+    fn from(t: &'a mut T) -> RMBA<'a, T> { RMBA::make(t as *const T, 3) }
 }
 
-impl<'a, T: 'a> From<Arc<T>> for RBA<'a, T> {
-    fn from(t: Arc<T>) -> RBA<'a, T> {
-        let z = RBA::make(&*t, 2);
+impl<'a, T: 'a> From<Box<T>> for RMBA<'a, T> {
+    fn from(t: Box<T>) -> RMBA<'a, T> { RMBA::make(Box::into_raw(t) as *const T, 1) }
+}
+
+impl<'a, T: 'a> From<Arc<T>> for RMBA<'a, T> {
+    fn from(t: Arc<T>) -> RMBA<'a, T> {
+        let z = RMBA::make(&*t, 2);
         forget(t);
         z
     }
 }
 
-impl<'a, T: 'a> fmt::Debug for RBA<'a, T> {
+impl<'a, T: 'a> fmt::Debug for RMBA<'a, T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "RBA({})", match self.0 & 3 { 0 => "&", 1 => "Box", 2 => "Arc", _ => unreachable!() })
+        write!(f, "RMBA({})", match self.0 & 3 { 0 => "&", 1 => "Box", 2 => "Arc", 3 => "&mut", _ => unreachable!() })
     }
 }
 
-
-impl<'a, T: 'a> Deref for RBA<'a, T> {
+impl<'a, T: 'a> Deref for RMBA<'a, T> {
     type Target = T;
     #[inline]
     fn deref(&self) -> &T { unsafe { transmute(self.0 & (!3)) }}
 }
 
-impl<'a, T: 'a> Drop for RBA<'a, T> {
+impl<'a, T: 'a> Drop for RMBA<'a, T> {
     fn drop(&mut self) {
         match self.0 & 3 {
-            0 => {},
+            0|3 => {},
             1 => unsafe { Box::<T>::from_raw(transmute(self.0-1)); },
             2 => unsafe { let _: Arc<T> = transmute(self.0 - 2 - 2*size_of::<AtomicUsize>()); },
             _ => unreachable!(),
@@ -67,40 +104,53 @@ impl<'a, T: 'a> Drop for RBA<'a, T> {
 }
 
 #[test]
-fn rba_arc() {
+fn rmba_box() {
+    let mut z = RMBA::new_box(78);
+    assert!(z.try_clone().is_none());
+    assert_eq!(*z, 78);
+    *z.get_mut().unwrap() = 73;
+    assert_eq!(*z, 73);
+}
+
+#[test]
+fn rmba_arc() {
     let mut a = Arc::new(53);
-    let f = RBA::new(a.clone());
+    let mut f = RMBA::new(a.clone());
     {
         let f2: &i32 = &f;
         assert_eq!(53, *f2);
     }
+    assert!(f.get_mut().is_none());
     assert!(Arc::get_mut(&mut a).is_none());
+    let z = f.try_clone().unwrap();
     drop(f);
+    assert!(Arc::get_mut(&mut a).is_none());
+    drop(z);
     assert!(Arc::get_mut(&mut a).is_some());
 }
 
 #[test]
 #[should_panic]
-fn rba_unaligned() {
+fn rmba_unaligned() {
     let b = vec![5u8, 7u8];
-    let _ = RBA::new(&b[0]); // Fails - u8 is not 32 bit aligned
+    let _ = RMBA::new(&b[0]); // Fails - u8 is not 32 bit aligned
 }
 
 #[test]
-fn rba_typical() {
+fn rmba_typical() {
     struct Dummy {
         a: Arc<i32>,
-        b: RBA<'static, i32>,
+        b: RMBA<'static, i32>,
     }
     let z = Arc::new(5i32);
-    let d = Dummy { a: z.clone(), b: RBA::new(z) };
+    let d = Dummy { a: z.clone(), b: RMBA::new(z) };
     assert_eq!(&*d.b, &5i32);
     assert_eq!(&*d.a, &5i32);
 }
 
 
 /// A simple wrapper around Box to avoid DerefMove. There is no way to get to
-/// &Box<T>, just &T. This way you can return a Bx<T> in your API and still be
+/// &Box<T>, just &T and &mut T. This way you can return a Bx<T> in your API and still be
 /// sure the inner struct does not move in memory.
 #[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct Bx<T>(Box<T>);

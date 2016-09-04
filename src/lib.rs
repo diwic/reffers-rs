adc6a46686803d1783a1445539fdd5c47a36e415
David Henningsson <diwic@ubuntu.com>
2016-09-04 14:28:28 +0200

    Make RMBA work with trait objects
    Signed-off-by: David Henningsson <diwic@ubuntu.com>

--- src/lib.rs
+++ src/lib.rs
@@ -6,22 +6,20 @@
 
 use std::sync::Arc;
 use std::marker::PhantomData;
-use std::mem::{transmute, forget, size_of, align_of};
 use std::ops::{Deref, DerefMut};
-use std::sync::atomic::AtomicUsize;
-use std::fmt;
+use std::{fmt, mem};
 
 /// Slightly bigger and slower than RMBA, but same functionality.
 /// Mostly used just to verify correctness of the real RMBA.
 #[derive(Debug)]
-pub enum SlowRMBA<'a, T: 'a> {
+pub enum SlowRMBA<'a, T: 'a + ?Sized> {
     Ref(&'a T),
     RefMut(&'a mut T),
     Box(Box<T>),
     Arc(Arc<T>),
 }
 
-impl<'a, T: 'a> SlowRMBA<'a, T> {
+impl<'a, T: 'a + ?Sized> SlowRMBA<'a, T> {
     /// Will return a clone if it contains a Arc<T> or &T, or None if it is a Box<T> or &mut T 
     pub fn try_clone(&self) -> Option<SlowRMBA<'a, T>> {
         match self {
@@ -43,7 +41,7 @@ impl<'a, T: 'a> SlowRMBA<'a, T> {
 }
 
 
-impl<'a, T: 'a> Deref for SlowRMBA<'a, T> {
+impl<'a, T: 'a + ?Sized> Deref for SlowRMBA<'a, T> {
     type Target = T;
     #[inline]
     fn deref(&self) -> &T {
@@ -58,33 +56,61 @@ impl<'a, T: 'a> Deref for SlowRMBA<'a, T> {
 
 
 /// RMBA can store an &T, a &mut T, a Box<T> or an Arc<T>.
-/// It will panic if you try to store something that's not 32 bit aligned.
+/// It will panic if you try to store a struct that's not 32 bit aligned.
 ///
 /// Note: Drop flags were removed in 1.13-nightly. If you run an earlier version,
 /// size might be larger than a single pointer due to the drop flag.
-pub struct RMBA<'a, T: 'a>(usize, PhantomData<SlowRMBA<'a, T>>);
+pub struct RMBA<'a, T: 'a + ?Sized>(*const T, PhantomData<SlowRMBA<'a, T>>);
+
 
 impl<'a, T: 'a> RMBA<'a, T> {
-    fn make(p: *const T, add: usize) -> RMBA<'a, T> {
-        let z: usize = unsafe { transmute(p) };
-        assert!(align_of::<T>() >= 4);
-        debug_assert!(z & 3 == 0);
-        RMBA(z+add, PhantomData)
+    pub fn new_box(t: T) -> RMBA<'a, T> { Box::new(t).into() }
+}
+
+impl<'a, T: 'a + ?Sized> RMBA<'a, T> {
+
+    #[inline]
+    fn unpack(&self) -> (*const T, usize) {
+        let mut p = self.0;
+        let f: *mut usize = (&mut p) as *mut _ as *mut usize;
+        unsafe {
+            let g = *f & 3;
+            *f = *f & (!3); 
+            (p, g)
+        }
+    }
+
+    fn make(mut p: *const T, add: usize) -> RMBA<'a, T> {
+        let f: *mut usize = (&mut p) as *mut _ as *mut usize;
+        debug_assert!(add <= 3);
+        unsafe {
+            assert!(*f & 3 == 0);
+            *f = *f + add;
+        }; 
+        RMBA(p, PhantomData)
+    }
+
+    // Turns an already unpacked *const T into an Arc<T>.
+    // Relies on how Arc is done internally, and no checking done!
+    // Must be forgotten after use!
+    unsafe fn arc(mut p: *const T) -> Arc<T> {
+        use std::sync::atomic::AtomicUsize;
+        let f: *mut usize = (&mut p) as *mut _ as *mut usize;
+        *f = *f - 2*mem::size_of::<AtomicUsize>();
+        mem::transmute(p)
     }
     
     pub fn new<A: Into<RMBA<'a, T>>>(t: A) -> RMBA<'a, T> { t.into() }
 
-    pub fn new_box(t: T) -> RMBA<'a, T> { Box::new(t).into() }
-
     /// Will return a clone if it contains a Arc<T> or &T, or None if it is a Box<T> or &mut T 
     pub fn try_clone(&self) -> Option<RMBA<'a, T>> {
-        match self.0 & 3 {
-            0 => Some(RMBA(self.0, PhantomData)),
-            2 => unsafe {
-                let t: Arc<T> = transmute(self.0 - 2 - 2*size_of::<AtomicUsize>());
-                let t2 = t.clone();
-                forget(t);
-                Some(t2.into())
+        match self.unpack() {
+            (_, 0) => Some(RMBA(self.0, PhantomData)),
+            (p, 2) => unsafe {
+                let a = Self::arc(p);
+                let r = Some(a.clone().into());
+                mem::forget(a);
+                r
             },
             _ => None,
         }
@@ -92,67 +118,83 @@ impl<'a, T: 'a> RMBA<'a, T> {
 
     /// Will return a &mut to inner contents if it contains a &mut T, a Box<T> or if the Arc<T> is unique. Otherwise returns None.
     pub fn get_mut(&mut self) -> Option<&mut T> {
-        match self.0 & 3 {
-            1 => unsafe {
-                let mut b = Box::<T>::from_raw(transmute(self.0-1));
+        match self.unpack() {
+            (p, 1) => unsafe {
+                let mut b = Box::<T>::from_raw(p as *mut T);
                 let z: *mut T = &mut *b;
-                forget(b);
+                mem::forget(b);
                 Some(&mut *z)
             },
-            2 => unsafe {
-                let mut t: Arc<T> = transmute(self.0 - 2 - 2*size_of::<AtomicUsize>());
-                let z: Option<*mut T> = Arc::get_mut(&mut t).map(|z| -> *mut T { &mut *z });
-                forget(t);
-                z.map(|z| &mut *z)
+            (p, 2) => unsafe {
+                let mut a = Self::arc(p);
+                let r = mem::transmute(Arc::get_mut(&mut a));
+                mem::forget(a);
+                r
             },
-            3 => unsafe { Some(transmute(self.0 - 3)) },
+            (p, 3) => unsafe { Some(&mut *(p as *mut T)) },
             _ => None,
         }
     }
+
 }
 
 
-impl<'a, T: 'a> From<&'a T> for RMBA<'a, T> {
+impl<'a, T: 'a + ?Sized> From<&'a T> for RMBA<'a, T> {
     fn from(t: &'a T) -> RMBA<'a, T> { RMBA::make(t as *const T, 0) }
 }
 
-impl<'a, T: 'a> From<&'a mut T> for RMBA<'a, T> {
+impl<'a, T: 'a + ?Sized> From<&'a mut T> for RMBA<'a, T> {
     fn from(t: &'a mut T) -> RMBA<'a, T> { RMBA::make(t as *const T, 3) }
 }
 
-impl<'a, T: 'a> From<Box<T>> for RMBA<'a, T> {
+impl<'a, T: 'a + ?Sized> From<Box<T>> for RMBA<'a, T> {
     fn from(t: Box<T>) -> RMBA<'a, T> { RMBA::make(Box::into_raw(t) as *const T, 1) }
 }
 
-impl<'a, T: 'a> From<Arc<T>> for RMBA<'a, T> {
+impl<'a, T: 'a + ?Sized> From<Arc<T>> for RMBA<'a, T> {
     fn from(t: Arc<T>) -> RMBA<'a, T> {
         let z = RMBA::make(&*t, 2);
-        forget(t);
+        // debug_assert_eq!(unsafe { Self::arc(z.unpack().0) }, &t as *const Arc<T>);
+        mem::forget(t);
         z
     }
 }
 
-impl<'a, T: 'a> fmt::Debug for RMBA<'a, T> {
+impl<'a, T: 'a + ?Sized> fmt::Debug for RMBA<'a, T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "RMBA({})", match self.0 & 3 { 0 => "&", 1 => "Box", 2 => "Arc", 3 => "&mut", _ => unreachable!() })
+        write!(f, "RMBA({})", match self.unpack().1 { 0 => "&", 1 => "Box", 2 => "Arc", 3 => "&mut", _ => unreachable!() })
     }
 }
 
-impl<'a, T: 'a> Deref for RMBA<'a, T> {
+impl<'a, T: 'a + ?Sized> Deref for RMBA<'a, T> {
     type Target = T;
     #[inline]
-    fn deref(&self) -> &T { unsafe { transmute(self.0 & (!3)) }}
+    fn deref(&self) -> &T { unsafe { &*self.unpack().0 }}
 }
 
-impl<'a, T: 'a> Drop for RMBA<'a, T> {
+impl<'a, T: 'a + ?Sized> Drop for RMBA<'a, T> {
     fn drop(&mut self) {
-        match self.0 & 3 {
-            0|3 => {},
-            1 => unsafe { Box::<T>::from_raw(transmute(self.0-1)); },
-            2 => unsafe { let _: Arc<T> = transmute(self.0 - 2 - 2*size_of::<AtomicUsize>()); },
+        match self.unpack() {
+            (_, 0) => {},
+            (p, 1) => unsafe { let _ = Box::<T>::from_raw(p as *mut T); },
+            (p, 2) => unsafe { let _ = Self::arc(p); },
+            (_, 3) => {},
             _ => unreachable!(),
         }
     }
+}
+
+#[test]
+fn rmba_fat() {
+    trait Dummy { fn foo(&self) -> i32; } 
+    impl Dummy for u8 { fn foo(&self) -> i32 { *self as i32 } }
+    impl Dummy for i32 { fn foo(&self) -> i32 { *self } }
+
+    let z: SlowRMBA<Dummy> = SlowRMBA::Box(Box::new(9u8));
+    let r: RMBA<Dummy> = (Box::new(9i32) as Box<Dummy>).into();
+    let a: RMBA<Dummy> = (Arc::new(17i32) as Arc<Dummy>).into();
+    assert_eq!(r.foo(), z.foo());
+    assert_eq!(a.try_clone().unwrap().foo(), 17i32);
 }
 
 #[test]
@@ -164,7 +206,7 @@ fn rmba_box() {
     assert_eq!(*z, 73);
 }
 
-#[test]
+#[test] 
 fn rmba_arc() {
     let mut a = Arc::new(53);
     let mut f = RMBA::new(a.clone());
@@ -185,7 +227,8 @@ fn rmba_arc() {
 #[should_panic]
 fn rmba_unaligned() {
     let b = vec![5u8, 7u8];
-    let _ = RMBA::new(&b[0]); // Fails - u8 is not 32 bit aligned
+    let _ = RMBA::new(&b[0]); // Either of these two will fail - u8 is not 32 bit aligned
+    let _ = RMBA::new(&b[1]); 
 }
 
 #[test]
@@ -202,6 +245,7 @@ fn rmba_typical() {
 
 #[test]
 fn rmba_sizes() {
+     use std::mem::size_of;
     assert_eq!(size_of::<&i32>(), size_of::<RMBA<i32>>());
     assert!(size_of::<RMBA<i32>>() < size_of::<SlowRMBA<i32>>());
 }
@@ -253,5 +297,4 @@ impl<T> DerefMut for Bxm<T> {
     #[inline]
     fn deref_mut(&mut self) -> &mut T { &mut self.0 }
 }
-
 

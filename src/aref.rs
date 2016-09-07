34a94d37fa5f2a3d521b5fc5f6fe697b1630f691
David Henningsson <diwic@ubuntu.com>
2016-09-07 10:33:13 +0200

    Improvements to ARef and docs
    Signed-off-by: David Henningsson <diwic@ubuntu.com>

--- src/aref.rs
+++ src/aref.rs
@@ -1,3 +1,5 @@
+
+use super::{RMBA, Bx, Bxm};
 use std::{ptr, mem};
 use std::ops::Deref;
 use std::rc::Rc;
@@ -39,9 +41,13 @@ pub fn verify_areffic<T: AReffic>(t: T) -> Result<T, & 'static str> {
 unsafe impl<T: ?Sized> AReffic for Box<T> {}
 unsafe impl<T: ?Sized> AReffic for Rc<T> {}
 unsafe impl<T: ?Sized> AReffic for Arc<T> {}
+unsafe impl<'a, T: ?Sized> AReffic for RMBA<'a, T> {}
+unsafe impl<T: ?Sized> AReffic for Bx<T> {}
+unsafe impl<T: ?Sized> AReffic for Bxm<T> {}
 unsafe impl<T> AReffic for Vec<T> {}
 unsafe impl AReffic for String {}
-unsafe impl<T: ?Sized> AReffic for &'static T {}
+unsafe impl<'a, T: ?Sized> AReffic for &'a T {}
+unsafe impl<'a, T: ?Sized> AReffic for &'a mut T {}
 unsafe impl<'a, T: ?Sized> AReffic for Ref<'a, T> {}
 unsafe impl<'a, T: ?Sized> AReffic for RefMut<'a, T> {}
 unsafe impl<'a, T: ?Sized> AReffic for RwLockReadGuard<'a, T> {}
@@ -49,23 +55,50 @@ unsafe impl<'a, T: ?Sized> AReffic for RwLockWriteGuard<'a, T> {}
 unsafe impl<'a, T: ?Sized> AReffic for MutexGuard<'a, T> {}
 
 
-// ARef - a reference that abstracts the owner completely.
-//
-// ARef takes over where OwningRef ends, by abstracting the owner even further.
-//
-// This makes it possible to return, say, an ARef<str> and have the caller drop the owner
-// when done looking at it, without having to bother about whether the owner is a String, Rc<String>, a
-// Ref<String>, or something else.
-//
-// Oh, and it's repr(C), so it can be transferred over an FFI boundary
-// (if its target is repr(C), too).
+/// ARef - a reference that abstracts the owner completely.
+///
+/// ARef takes over where [OwningRef](https://crates.io/crates/owning_ref) ends, by abstracting the owner even further.
+///
+/// This makes it possible to return, say, an `ARef<str>` and have the caller drop the owner
+/// when done looking at it, without having to bother about whether the owner is a `String`, `Rc<String>`, a
+/// `Ref<String>`, or something else.
+///
+/// Oh, and it's repr(C), so it can be transferred over an FFI boundary
+/// (if its target is repr(C), too).
+///
+/// # Example
+/// ```
+/// use std::rc::Rc;
+/// use reffers::ARef;
+/// 
+/// struct CountDown(pub Rc<String>);
+/// impl CountDown {
+///     pub fn idx_to_str(&self, idx: u32) -> ARef<str> {
+///         match idx {
+///             0 => "Go!".into(),
+///             // We clone the Rc instead of the String
+///             // for performance,
+///             // then we map from &String to &str
+///             1 => ARef::new(self.0.clone()).map(|s| &**s),
+///             _ => format!("{}...", idx).into(),
+///         }
+///     }
+/// }
+/// 
+/// let c = CountDown(Rc::new("Ready!".into()));
+/// assert_eq!(&*c.idx_to_str(3), "3...");
+/// assert_eq!(&*c.idx_to_str(2), "2...");
+/// assert_eq!(&*c.idx_to_str(1), "Ready!");
+/// assert_eq!(&*c.idx_to_str(0), "Go!");
+/// ```
+
 #[repr(C)]
-pub struct ARef<U: ?Sized> {
+pub struct ARef<'a, U: ?Sized> {
+    target: *const U,
     dropfn: unsafe fn (*mut ARefStorage),
     owner: ARefStorage,
-    target: *const U,
     // Just to be 100% to disable Send and Sync
-    _dummy: PhantomData<Rc<()>>
+    _dummy: PhantomData<(Rc<()>, &'a ())>
 }
 
 // We can't call drop_in_place directly, see https://github.com/rust-lang/rust/issues/34123
@@ -74,7 +107,7 @@ unsafe fn aref_drop_wrapper<T>(t: *mut ARefStorage) {
 }
 
 
-impl<U: ?Sized> Drop for ARef<U> {
+impl<'a, U: ?Sized> Drop for ARef<'a, U> {
     fn drop(&mut self) {
         unsafe {
             (self.dropfn)(&mut self.owner);
@@ -82,21 +115,22 @@ impl<U: ?Sized> Drop for ARef<U> {
     }
 }
 
-impl<U: ?Sized> Deref for ARef<U> {
+impl<'a, U: ?Sized> Deref for ARef<'a, U> {
     type Target = U;
 
+    #[inline]
     fn deref(&self) -> &U {
         unsafe { &*self.target }
     }
 }
 
-impl<U: ?Sized> AsRef<U> for ARef<U> {
+impl<'a, U: ?Sized> AsRef<U> for ARef<'a, U> {
     fn as_ref(&self) -> &U {
         &*self
     }
 }
 
-impl<U: ?Sized> ARef<U> {
+impl<'a, U: ?Sized> ARef<'a, U> {
     /// Creates a new ARef from what the ARef points to.
     ///
     /// # Example
@@ -108,14 +142,37 @@ impl<U: ?Sized> ARef<U> {
     /// assert_eq!(*aref, 43);
     /// ```
     pub fn new<O>(owner: O) -> Self
-        where O: AReffic,
-              O: Deref<Target = U>,
+        where O: 'a + AReffic + Deref<Target = U>
     {
         owner.into()
     }
+
+    /// Maps the ARef's target to something reachable from the target.
+    ///
+    /// # Example
+    /// ```
+    /// use std::rc::Rc;
+    /// use reffers::ARef;
+    ///
+    /// let aref: ARef<[u8]> = vec![0u8, 5, 7].into();
+    /// assert_eq!(*aref.map(|s| &s[1]), 5);
+    /// ```
+    pub fn map<V: ?Sized, F: FnOnce(&U) -> &V>(self, f: F) -> ARef<'a, V>
+    {
+        let t: *const V = f(&self);
+        let o = self.owner;
+        let d = self.dropfn;
+        mem::forget(self);
+        ARef {
+            target: t,
+            owner: o,
+            dropfn: d,
+            _dummy: PhantomData,
+        }
+    }
 }
 
-impl<O: AReffic + Deref<Target = U>, U: ?Sized> From<O> for ARef<U>
+impl<'a, O: 'a + AReffic + Deref<Target = U>, U: ?Sized> From<O> for ARef<'a, U>
 {
     fn from(owner: O) -> Self {
         let mut o2 = owner;
@@ -146,7 +203,6 @@ fn verify_drop() {
     assert!(Rc::get_mut(&mut z).is_some());
 }
 
-
 #[test]
 fn verify_types() {
     use std::cell::RefCell;
@@ -154,6 +210,9 @@ fn verify_types() {
     verify_areffic(Box::new(5u8)).unwrap();
     verify_areffic(Rc::new(5u8)).unwrap();
     verify_areffic(Arc::new(5u8)).unwrap();
+    verify_areffic(Bx::new(5u8)).unwrap();
+    verify_areffic(Bxm::new(5u8)).unwrap();
+    verify_areffic(RMBA::from(Arc::new(5u8))).unwrap();
     verify_areffic(String::from("Hello aref")).unwrap();
     verify_areffic(vec![4711]).unwrap();
     verify_areffic("This is areffic").unwrap();
@@ -165,5 +224,38 @@ fn verify_types() {
     assert_eq!(*verify_areffic(r.read().unwrap()).unwrap(), 5u8);
     let m = Mutex::new(5u8);
     assert_eq!(*verify_areffic(m.lock().unwrap()).unwrap(), 5u8);
+}
+
+/*
+fn compile_fail<'a>() -> ARef<'a, str> {
+    let z = String::from("Hello world");
+    let z2: &str = &z; 
+    z2.into()
+}
+
+fn compile_fail2<'a>() -> ARef<'a, [&'a u8]> {
+    let z = vec![5u8, 4, 3];
+    let z2 = vec![&z[0], &z[2]]; 
+    z2.into()
+}
+*/
+
+#[test]
+fn countdown_example() {
+    struct CountDown(pub Rc<String>);
+    impl CountDown {
+        pub fn idx_to_str(&self, idx: u32) -> ARef<str> {
+            match idx {
+                0 => "Go!".into(),
+                1 => ARef::new(self.0.clone()).map(|s| &**s),
+                _ => format!("{}...", idx).into(),
+            }
+        }
+    }
+
+    let c = CountDown(Rc::new(String::from("Ready!")));
+    assert_eq!(&*c.idx_to_str(0), "Go!");
+    assert_eq!(&*c.idx_to_str(1), "Ready!");
+    assert_eq!(&*c.idx_to_str(2), "2...");
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 //! Wrappers around references, boxes or Arcs.
 //!
 //! Features: 
-//! * Bx and Bxm: Boxes without DerefMove.
+//!
+//! * ARef: OwningRef with even further erasure of the owner.
+//!
 //! * RMBA: Wrap a &T, &mut T, Box<T> or Arc<T> within the size of a single pointer. 
+//!
+//! * Bx and Bxm: Boxes without DerefMove.
 
 
 use std::sync::Arc;
@@ -255,21 +259,24 @@ fn rmba_sizes() {
     assert!(size_of::<RMBA<i32>>() < size_of::<SlowRMBA<i32>>());
 }
 
-/// A simple wrapper around Box to avoid DerefMove. There is no way to get to
+/// A simple wrapper around Box to avoid DerefMove.
+///
+/// There is no way to get to
 /// &Box<T>, just &T. This way you can return a Bx<T> in your API and still be
-/// sure the inner struct does not move in memory.
+/// sure the inner struct does not move in memory. (This might be helpful if you're
+/// dealing with FFI or unsafe code.)
 #[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
-pub struct Bx<T>(Box<T>);
+pub struct Bx<T: ?Sized>(Box<T>);
 
 impl<T> Bx<T> {
     pub fn new(t: T) -> Bx<T> { Bx(Box::new(t)) }
 }
 
-impl<T> From<Box<T>> for Bx<T> {
+impl<T: ?Sized> From<Box<T>> for Bx<T> {
     fn from(t: Box<T>) -> Bx<T> { Bx(t) }
 }
 
-impl<T> Deref for Bx<T> {
+impl<T: ?Sized> Deref for Bx<T> {
     type Target = T;
     #[inline]
     fn deref(&self) -> &T { &self.0 }
@@ -282,23 +289,23 @@ impl<T> Deref for Bx<T> {
 /// `mem::replace` to move the box's contents to other memory
 /// locations.
 #[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
-pub struct Bxm<T>(Box<T>);
+pub struct Bxm<T: ?Sized>(Box<T>);
 
 impl<T> Bxm<T> {
     pub fn new(t: T) -> Bxm<T> { Bxm(Box::new(t)) }
 }
 
-impl<T> From<Box<T>> for Bxm<T> {
+impl<T: ?Sized> From<Box<T>> for Bxm<T> {
     fn from(t: Box<T>) -> Bxm<T> { Bxm(t) }
 }
 
-impl<T> Deref for Bxm<T> {
+impl<T: ?Sized> Deref for Bxm<T> {
     type Target = T;
     #[inline]
     fn deref(&self) -> &T { &self.0 }
 }
 
-impl<T> DerefMut for Bxm<T> {
+impl<T: ?Sized> DerefMut for Bxm<T> {
     #[inline]
     fn deref_mut(&mut self) -> &mut T { &mut self.0 }
 }
